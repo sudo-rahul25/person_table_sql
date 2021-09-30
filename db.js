6c8335cee3f526e48b5075ad13cee3f603492544
Omkar Deshmukh <omkarvd7066@gmail.com>
2021-09-30 22:29:35 +0530

    Formatted Codes

--- db.js
+++ db.js
@@ -1,47 +1,44 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'myuser',
-    password: 'mypass',
-    database: 'mytestdb'
+    host: "localhost",
+    user: "myuser",
+    password: "mypass",
+    database: "mytestdb",
 });
 
-function getall(){
-    return new Promise(function(resolve, reject){
-        connection.query(
-            `select * from persons`,
-            function(err, rows, cols){
-                if(err)reject(err);
-                else resolve(rows);
-            }
-        )
-    })
-};
+function getall() {
+    return new Promise(function (resolve, reject) {
+        connection.query(`select * from persons`, function (err, rows, cols) {
+            if (err) reject(err);
+            else resolve(rows);
+        });
+    });
+}
 
-function putin(name, age, city){
-    return new Promise(function(resolve, reject){
+function putin(name, age, city) {
+    return new Promise(function (resolve, reject) {
         connection.query(
             `insert into persons (name, age, city) values (?, ?, ?)`,
             [name, age, city],
-            function(err){
-                if(err) reject(err);
+            function (err) {
+                if (err) reject(err);
                 else resolve();
-            }    
-        )
-    })
-};
+            }
+        );
+    });
+}
 
-function delfrom(name){
-    return new Promise(function(resolve, reject){
+function delfrom(name) {
+    return new Promise(function (resolve, reject) {
         connection.query(
             `delete from persons where name = ?`,
             [name],
-            function(err){
-                if(err)reject(err);
+            function (err) {
+                if (err) reject(err);
                 else resolve();
             }
-        )
-    })
+        );
+    });
 }
 
-module.exports = {get: getall, put: putin, del: delfrom};
+module.exports = { get: getall, put: putin, del: delfrom };
--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
-$(function(){
-    let inp_add_name = $('#addname');
-    let inp_del_name = $('#delname');
-    let inp_age = $('#age');
-    let inp_city = $('#city');
-    let btn_add = $('#add');
-    let btn_del = $('#del');
-    let tbl_persons = $('#persons');
-    function refresh(persons){
+$(function () {
+    let inp_add_name = $("#addname");
+    let inp_del_name = $("#delname");
+    let inp_age = $("#age");
+    let inp_city = $("#city");
+    let btn_add = $("#add");
+    let btn_del = $("#del");
+    let tbl_persons = $("#persons");
+    function refresh(persons) {
         tbl_persons.empty();
         tbl_persons.append(
             `
@@ -17,7 +17,7 @@ $(function(){
             </tr>
             `
         );
-        for(person of persons){
+        for (person of persons) {
             tbl_persons.append(
                 `
                 <tr>
@@ -27,30 +27,34 @@ $(function(){
                 </tr>
                 `
             );
-        };
+        }
     }
-    $.get('/api/persons', function(data){
+    $.get("/api/persons", function (data) {
         refresh(data);
-    })
-    btn_add.click(function(){
-        $.post('/api/persons/add',
-        {
-            name: inp_add_name.val(),
-            age: inp_age.val(),
-            city: inp_city.val()
-        },
-        function(data){
-            refresh(data);
-        })
-    })
-    btn_del.click(function(){
+    });
+    btn_add.click(function () {
+        $.post(
+            "/api/persons/add",
+            {
+                name: inp_add_name.val(),
+                age: inp_age.val(),
+                city: inp_city.val(),
+            },
+            function (data) {
+                refresh(data);
+            }
+        );
+    });
+    btn_del.click(function () {
         console.log(inp_del_name.val());
-        $.post('/api/persons/del',
-        {
-            name: inp_del_name.val()
-        },
-        function(data){
-            refresh(data);
-        })
-    })
+        $.post(
+            "/api/persons/del",
+            {
+                name: inp_del_name.val(),
+            },
+            function (data) {
+                refresh(data);
+            }
+        );
+    });
 });
